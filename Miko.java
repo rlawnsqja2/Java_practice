30ccca38faeff1f95bd60fab4863df77695633dc
김준범 <127375397+rlawnsqja2@users.noreply.github.com>
2023-10-31 18:15:12 +0900

    Add files via upload

---
+++ Miko.java
@@ -0,0 +1,13 @@
+package narukami;
+
+public class Miko {
+	String name;
+	int Dog;
+	public Miko(String n,int d) {
+		name=n;
+		Dog=d;
+	}
+	public void bite() {
+		System.out.println(name+"이(가) 미코를 물었습니다.");
+	}
+}
